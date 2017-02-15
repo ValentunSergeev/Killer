154532a04e0c277552c7f2a91ee77e156d8546b6
Valentun <karbon4ic@gmail.com>
2017-02-15 21:14:29 +0600

    Change user model
    Add image and name column to users, add user image to navbar, remove
    email auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable,
          :validatable, :omniauthable, omniauth_providers: [:vkontakte]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      # user.nsame = auth.info.name   # assuming the user model has a name
-      #  user.image = auth.info.image   add base64 encoding
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.name = auth.info.name
+      user.image = auth.info.image
       user.save!
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214112216) do
+ActiveRecord::Schema.define(version: 20170215141350) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20170214112216) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
